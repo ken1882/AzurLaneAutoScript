23f5407cb7d3fc5f561fdca6130b589120d193fd
Compeador <qj6w94g0q@gmail.com>
2025-10-24 02:24:51 +0800

    chore: local

--- module/island/interact.py
+++ module/island/interact.py
@@ -12,50 +12,5 @@ class IslandInteract(UI):
     def goto_ui(self, target_page):
         return self.ui_goto(target_page, get_ship=False)
 
-    def dismiss(self, repeats=1):
-        """
-        Dismiss any popups if exist
-        """
-        for _ in range(repeats):
-            self.device.click(self.BTN_EMPTY)
-            self.device.sleep(0.1)
-
-    def click_and_wait_until_appear(self,
-                                    btn: Button,
-                                    *targets: list,
-                                    threshold=20,
-                                    similarity=0.85,
-                                    timeout=Timer(5, 10),
-                                    interval=1
-                                    ):
-        """
-        Click a button and wait until one of given target appear
-
-        Args:
-            btn: Button to click
-            targets: List of target template or button to wait for
-            threshold: Matching threshold for button
-            similarity: Similarity threshold for target
-            timeout: Timeout timer
-            interval: Interval between click and check
-        Returns:
-            The target button appeared, or None if timeout
-        """
-        timeout.reset()
-        while 1:
-            if timeout.reached():
-                logger.error(f'Timeout waiting for {",".join([t.name for t in targets])} to appear')
-                return False
-            self.device.click(btn)
-            self.device.sleep(interval)
-            self.device.screenshot()
-            for t in targets:
-                if isinstance(t, Button):
-                    if self.appear(t, threshold=threshold, similarity=similarity):
-                        return t
-                elif isinstance(t, Template):
-                    sim, btn = t.match_luma_result(self.device.image)
-                    if sim >= similarity:
-                        return btn
-                else:
-                    logger.warning(f'Invalid target type: {type(t)}')
+    def dismiss(self):
+        self.device.click(self.BTN_EMPTY)
